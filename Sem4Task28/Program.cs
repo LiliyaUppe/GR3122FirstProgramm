170cadbeba42f7638ddfaee9dcc134d7241fd51a
Liliya Uppe <lilya30rus@mail.ru>
2022-09-12 15:30:54 +0200

    add Sem4Task28

---
+++ Sem4Task28/Program.cs
@@ -0,0 +1,38 @@
+﻿// #28 Напишите программу, которая принимает на вход
+// число N и выдаёт произведение чисел от 1 до N.
+// Например:
+// 4 -> 24
+// 5 -> 120
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    // Выводим сообщение
+    Console.WriteLine(line);
+    // Считываем число
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    // Возвращаем значение
+    return number;
+}
+
+// Печать результата
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+//произведение чисел от 1 до N - это факториал N!=1*2*3*...*N
+
+long CalculateFacktorial(int number)
+{
+    long factorial = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        factorial = factorial * i;
+    }
+    return factorial;
+}
+int number = ReadData("Input number: ");
+long factorial = CalculateFacktorial(number);
+PrintResult($"N! = {factorial}");
+
