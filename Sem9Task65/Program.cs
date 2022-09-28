11cdbc6916308807766499aef8a36678bc2aace7
Liliya Uppe <lilya30rus@mail.ru>
2022-09-28 20:28:34 +0200

    add solution Sem9Task63, Sem9Task64,Sem9Task65, Sem9Task67, Sem9Task68, Sem9Task69

---
+++ Sem9Task65/Program.cs
@@ -0,0 +1,39 @@
+﻿// #65 Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Метод, принимает строку, выводит в консоль
+void PrintResult(int numberM,int numberN )
+{
+    if  (numberM< numberN) 
+    {
+       Console.WriteLine($"Сумма в промежутке M...N = {SumRecMN(numberM,numberN)}");
+    }
+    if (numberM> numberN) 
+    {
+        Console.WriteLine ( $"Сумма в промежутке M...N = {SumRecMN(numberN, numberM)}");
+    }
+}
+
+int SumRecMN(int numberM, int numberN)
+{
+    if (numberM >= numberN) return numberN;
+   return  numberM + SumRecMN(numberM + 1, numberN);
+}
+
+
+int numberM = ReadData("Введите числа M: ");
+int numberN = ReadData("Введите числа N: ");
+
+PrintResult(numberM, numberN);
---
+++ Sem9Task68/Program.cs
@@ -0,0 +1,32 @@
+﻿// #68 Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+//функция Аккермана
+int Akkerman(int numberM, int numberN)
+{
+    if (numberM == 0)
+    return numberN + 1;
+  else
+    if ((numberN == 0) && (numberM > 0))
+      return Akkerman(numberM - 1, 1);
+    else
+      return Akkerman(numberM - 1, Akkerman(numberM, numberN - 1));
+}
+
+
+int numberM = ReadData("Введите числа m: ");
+int numberN = ReadData("Введите числа n: ");
+int result = Akkerman(numberM, numberN);
+
+Console.Write($"m = {numberM}, n = {numberN} -> A(m,n) = {result}");
+
